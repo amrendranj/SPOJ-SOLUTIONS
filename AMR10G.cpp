4abb510302339d096c3301148a0b1d216e7125fb
CodeBuddyKira <amrendrajha65@gmail.com>
2018-08-02 17:32:29 +0530

    AMR10G Added

---
+++ AMR10G.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int A[1000000];
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        for(int i=0;i<n;i++){
+            cin>>A[i];
+        }
+        sort(A,A+n);
+        int result = 1000000000;
+        for(int i=k-1;i<n;i++){
+            result = min(result, (A[i]-A[i-k+1]));
+        }
+        cout<<result<<endl;
+    }
+}
