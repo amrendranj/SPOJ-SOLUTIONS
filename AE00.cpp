8de64868f069b23b38433e08f57784d0aa77cbd5
CodeBuddyKira <amrendrajha65@gmail.com>
2018-08-02 14:48:38 +0530

    Completed AE00

--- AE00.cpp
+++ AE00.cpp
@@ -1,5 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int n;
+    int result = 0;
+    cin>>n;
+    for(int i=1;i<=sqrt(n);i++){
+        result+= ((n/i) - i + 1);
+    }
+    cout<<result<<endl;
 }
